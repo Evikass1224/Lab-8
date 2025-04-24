69290d96eb0fcd461eba6809276ff7069fc61e83
Evika <evelinashubina389@gmail.com>
2025-04-24 12:00:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Planner
+{
+    /// <summary>
+    /// Класс является точкой входа в приложение для управления задачами.
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            bool exitFlag;
+
+            TaskManager taskManager = new TaskManager();
+            taskManager.Menu(out exitFlag);
+
+            if (exitFlag)
+            {
+                Console.WriteLine("Выход из программы.");
+            }
+        }
+    }
+}
